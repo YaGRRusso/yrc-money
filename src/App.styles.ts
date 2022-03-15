d403f95e3e5b23f6e85123e28f3a9fdabedd7c97
Yago Russo <yagrrusso@gmail.com>
2022-03-15 02:59:05 -0300

    primeiro commit

---
+++ src/App.styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: red;
+  font-size: 16px;
+`
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as C from './App.styles'
+
+const App = () => {
+  return (
+    <C.Container>
+      Hello World
+    </C.Container>
+  )
+}
+
+export default App;
